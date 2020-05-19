a11191c1e940c2ad5c116b6d9a4f25a74165643e
Carolina Nunes Pinheiro <nunespcarolina@gmail.com>
2020-05-19 18:19:30 -0300

    Leitura do volume

--- prova.cpp
+++ prova.cpp
@@ -2,12 +2,17 @@
 
 using namespace std;
 int main() {
+	int  a;
+    double b;
 	
-	
-	
-	cout << "SISTEMA DE PERFIL DE CLIENTE" << endl;
-   	cout << "--------------------------------" << endl;
-	cout << "INFORME OS DADOS DO ÚLTIMO ANO" << endl;
+	cout << "SISTEMA DE PERFIL DE CLIENTE" << endl ;
+   	cout << "--------------------------------" << endl ;
+	cout << "INFORME OS DADOS DO ULTIMO ANO" << endl ;
+    
+	cout << "Quantas compras o cliente fez no ultimo ano?" ;
+	cin >> a;
+	cout << "Qual o ticket medio?";
+	cin >> b;
 
 	
 	 
